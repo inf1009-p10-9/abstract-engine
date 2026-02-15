9b5608d893ebabb1ceb75d0239e759df263812d0
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2026-02-15 17:25:40 +0800

    feat: refactor IMovable to IPositionable
    To align with 13-02-2026 feedback
    
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- lwjgl3/src/main/java/io/github/inf1009_p10_9/interfaces/IMovementStrategy.java
+++ lwjgl3/src/main/java/io/github/inf1009_p10_9/interfaces/IMovementStrategy.java
@@ -3,5 +3,5 @@ package io.github.inf1009_p10_9.interfaces;
 import io.github.inf1009_p10_9.entities.Entity;
 
 public interface IMovementStrategy {
-    void calculateMovement(Entity object, int moveCount, IMovable movable);
+    void calculateMovement(IPositionable object, int moveCount);
 }
--- lwjgl3/src/main/java/io/github/inf1009_p10_9/managers/EntityManager.java
+++ lwjgl3/src/main/java/io/github/inf1009_p10_9/managers/EntityManager.java
@@ -4,57 +4,56 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Queue;
 import io.github.inf1009_p10_9.entities.Entity;
-import io.github.inf1009_p10_9.interfaces.IMovable;
 
-public class EntityManager implements IMovable {
+public class EntityManager {
     private static EntityManager instance;
-    
+
     private Array<Entity> entities;
     private Queue<Entity> entitiesToAdd;
     private Queue<Entity> entitiesToRemove;
-    
+
     private EntityManager() {
-        entities = new Array<>();
-        entitiesToAdd = new Queue<>();
-        entitiesToRemove = new Queue<>();
+        entities = new Array<Entity>();
+        entitiesToAdd = new Queue<Entity>();
+        entitiesToRemove = new Queue<Entity>();
     }
-    
+
     public static EntityManager getInstance() {
         if (instance == null) {
             instance = new EntityManager();
         }
         return instance;
     }
-    
+
     public void initialize() {
         entities.clear();
         entitiesToAdd.clear();
         entitiesToRemove.clear();
     }
-    
+
     public void addEntity(Entity entity) {
         entitiesToAdd.addLast(entity);
     }
-    
+
     public void removeEntity(Entity entity) {
         entitiesToRemove.addLast(entity);
     }
-    
+
     public Array<Entity> getEntities() {
         return entities;
     }
-    
+
     public void update() {
         // Process pending additions
         while (entitiesToAdd.size > 0) {
             entities.add(entitiesToAdd.removeFirst());
         }
-        
+
         // Process pending removals
         while (entitiesToRemove.size > 0) {
             entities.removeValue(entitiesToRemove.removeFirst(), true);
         }
-        
+
         // Update all entities
         for (Entity entity : entities) {
             if (entity.isActive()) {
@@ -62,19 +61,14 @@ public class EntityManager implements IMovable {
             }
         }
     }
-    
+
     public void clear() {
         entities.clear();
         entitiesToAdd.clear();
         entitiesToRemove.clear();
     }
-    
+
     public int getEntityCount() {
         return entities.size;
-    }
-    
-    @Override
-    public void moveEntity(Entity entity, Vector2 newPosition) {
-        entity.setPosition(newPosition);
     }
 }
